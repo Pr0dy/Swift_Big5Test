88352ac6253b6549d6098ab895a4e56ed3c8960b
Leandro <leandro.miguel.banha@gmail.com>
2022-04-27 12:17:01 +0100

    added QuestionDatabase struct

---
+++ Big5Quiz/Models/QuestionDatabase.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct Extraversion{
+    //let extraversionQuestions = []
+    //let agreeablenessQuestions = []
+    //let conscientiounessQuestions = []
+    //let neuroticismQuestions = []
+    //let openess = []
+}
