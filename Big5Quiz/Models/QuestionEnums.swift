86d3609937c75dbd9b053f9e772f7d19732f7f08
Leandro <leandro.miguel.banha@gmail.com>
2022-04-27 12:29:01 +0100

    added enums for traits and subtraits

---
+++ Big5Quiz/Models/QuestionEnums.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+enum Trait{
+    case Extraversion
+    case Agreeableness
+    case Conscientiousness
+    case Neuroticism
+    case Openess
+}
+
+enum Subtraits{
+    //Extraversion Subtraits
+    case Activity
+    case Assertiveness
+    case Cheerfulness
+    case Excitement_Seeking
+    case Friendliness
+    case Gregariouness
+    //Agreeableness Subtraits
+    case Altruism
+    case Cooperation
+    case Modesty
+    case Morality
+    case Sympathy
+    case Trust
+    //Conscientiousness Subtraits
+    case Achievement_Striving
+    case Cautiousness
+    case Dutifulness
+    case Orderliness
+    case Self_Discipline
+    case Self_Efficacy
+    //Neuroticism Subtraits
+    case Anger
+    case Anxiety
+    case Depression
+    case Immoderation
+    case Self_Consciouness
+    case Vulnerability
+    //Openess Subtraits
+    case Adventurousness
+    case Artistic_Interests
+    case Emotionality
+    case Imagination
+    case Intellect
+    case Liberalism
+}
