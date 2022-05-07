87065c69bd472b3e2963a824e6b44cf751448a42
Leandro <leandro.miguel.banha@gmail.com>
2022-05-07 17:10:42 +0100

    calculates subtrait average

--- Big5Quiz/Models/Question.swift
+++ Big5Quiz/Models/Question.swift
@@ -4,11 +4,12 @@ class Question{
     let questionContent: String
     let questionTrait: Trait
     let questionSubTrait: Subtrait
+    let isPositive: Bool
     
-    init(content: String, trait: Trait, Subtrait: Subtrait){
+    init(content: String, trait: Trait, Subtrait: Subtrait, positiveQuestion: Bool){
         self.questionContent = content
         self.questionTrait = trait
         self.questionSubTrait = Subtrait
+        self.isPositive = positiveQuestion
     }
 }
-
--- Big5Quiz/Models/QuizTestScores.swift
+++ Big5Quiz/Models/QuizTestScores.swift
@@ -1,32 +1,55 @@
 import Foundation
 
 struct QuizTestScores{
-    
     var Extraversion = ExtraversionScore()
     var Agreeableness =  AgreeablenessScore()
     var Conscientiousness = ConscientiousnessScore()
     var Neuroticism = NeuroticismScore()
     var Openess = OpenessScore()
-    
 }
 
 struct ExtraversionScore{
     var activityScore = 0, sympathyScore = 0, cheerfulnessScore = 0,  excitementSeekingScore = 0, friendlinessScore = 0, gregariounessScore = 0
+    var traitScore: Int {
+        get{
+            return (activityScore + sympathyScore + cheerfulnessScore + excitementSeekingScore + friendlinessScore + gregariounessScore / 6) * 100
+        }
+    }
 }
 
 struct AgreeablenessScore{
     var altruismScore = 0, cooperationScore = 0, modestyScore = 0, moralityScore = 0, assertivenessScore = 0 ,trustScore = 0
+    var traitScore: Int {
+        get{
+            return (altruismScore + cooperationScore + modestyScore + moralityScore + assertivenessScore + trustScore / 6) * 100
+        }
+    }
 }
 
 struct ConscientiousnessScore{
-    var achievementStrivingScore = 0, CcutiousnessScore = 0, dutifulnessScore = 0, orderlinessScore = 0, selfDisciplineScore = 0, selfEfficacyScore = 0
+    var achievementStrivingScore = 0, cautiousnessScore = 0, dutifulnessScore = 0, orderlinessScore = 0, selfDisciplineScore = 0, selfEfficacyScore = 0
+    var traitScore: Int {
+        get{
+            return (achievementStrivingScore + cautiousnessScore + dutifulnessScore + orderlinessScore + selfDisciplineScore + selfEfficacyScore / 6) * 100
+        }
+    }
 }
 
 struct NeuroticismScore{
     var angerScore = 0, anxietyScore = 0, depressionScore = 0, immoderationScore = 0, selfConsciounessScore = 0, vulnerabilityScore = 0
+    var traitScore: Int {
+        get{
+            return (angerScore + anxietyScore + depressionScore + immoderationScore + selfConsciounessScore + vulnerabilityScore / 6) * 100
+        }
+    }
 }
 
 struct OpenessScore{
     var adventurousnessScore = 0, artisticInterestsScore = 0, emotionalityScore = 0, imaginationScore = 0, intelectScore = 0, liberalismScore = 0
+    var traitScore: Int {
+        get{
+            return (adventurousnessScore + artisticInterestsScore + emotionalityScore + imaginationScore + intelectScore + liberalismScore / 6) * 100
+        }
+    }
 }
 
