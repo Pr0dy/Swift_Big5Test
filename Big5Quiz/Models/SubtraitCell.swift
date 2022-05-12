756b8a4775ae893580614893a53ffdeea5b2cf17
Leandro <leandro.miguel.banha@gmail.com>
2022-05-12 13:49:48 +0100

    added subtrait cell

---
+++ Big5Quiz/Models/SubtraitCell.swift
@@ -0,0 +1,23 @@
+//
+//  SubtraitCell.swift
+//  Big5Quiz
+//
+//  Created by leandro.banha on 12/05/2022.
+//
+
+import UIKit
+
+class SubtraitCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
