5e9dc365be357190c2b858f7fe450ff25fbe2e6a
Leandro <leandro.miguel.banha@gmail.com>
2022-06-02 11:06:11 +0100

    removed unused files

--- Big5Quiz/Supporting Files/CustomCell.swift
+++ Big5Quiz/Supporting Files/CustomCell.swift
@@ -1,20 +1,27 @@
+//
+//  CustomCell.swift
+//  Big5Quiz
+//
+//  Created by leandro.banha on 02/06/2022.
+//
+
 import UIKit
 
-class ResultCell: UITableViewCell {
+class CustomCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    @IBOutlet weak var downImage: UIImageView!
     @IBOutlet weak var traitScore: UILabel!
     @IBOutlet weak var traitName: UILabel!
     @IBOutlet weak var traitDescription: UILabel!
-    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
-    
+
 }
